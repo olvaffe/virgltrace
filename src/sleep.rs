8312931cccfe6ac27294af550d39039382374fa5
Chia-I Wu <olvaffe@gmail.com>
2019-07-15 20:11:12 -0700

    handle ctrl-c

--- src/sleep.rs
+++ src/sleep.rs
@@ -1,5 +1,31 @@
-use std::{thread, time};
+extern crate libc;
+
+use std::sync::{Arc, Condvar, Mutex};
+use std::time;
+
+static mut SIGNAL: Option<Arc<(Mutex<()>, Condvar)>> = None;
+
+unsafe extern "C" fn signal_handler(_: libc::c_int) {
+    let signal = SIGNAL.as_ref().unwrap();
+    let _guard = signal.0.lock().unwrap();
+    signal.1.notify_one();
+}
 
 pub fn sleep(secs : u32) {
-    thread::sleep(time::Duration::from_secs(secs.into()));
+    let dur = time::Duration::from_secs(secs.into());
+
+    let signal = Arc::new((Mutex::new(()), Condvar::new()));
+    let guard = signal.0.lock().unwrap();
+
+    unsafe {
+        SIGNAL = Some(signal.clone());
+        libc::signal(libc::SIGINT, signal_handler as usize);
+    }
+
+    let _ = signal.1.wait_timeout(guard, dur);
+
+    unsafe {
+        libc::signal(libc::SIGINT, libc::SIG_DFL);
+        SIGNAL = None;
+    }
 }
